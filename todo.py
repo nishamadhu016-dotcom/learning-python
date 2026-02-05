31a5a1de73e8ac122859f5900c91f1d76017881c
nishamadhu016-dotcom <nishamadhu016@gmail.com>
2026-02-05 14:22:03 +0530

    Create todo.py

---
+++ todo.py
@@ -0,0 +1,41 @@
+tasks = []
+
+while True:
+    print("\n--- TO DO LIST ---")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        task = input("Enter task: ")
+        tasks.append(task)
+        print("Task added successfully!")
+
+    elif choice == '2':
+        if not tasks:
+            print("No tasks available.")
+        else:
+            print("\nYour Tasks:")
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}")
+
+    elif choice == '3':
+        if not tasks:
+            print("No tasks to delete.")
+        else:
+            num = int(input("Enter task number to delete: "))
+            if 1 <= num <= len(tasks):
+                tasks.pop(num - 1)
+                print("Task deleted!")
+            else:
+                print("Invalid task number.")
+
+    elif choice == '4':
+        print("Exiting... Bye 👋")
+        break
+
+    else:
+        print("Invalid choice. Try again.")
